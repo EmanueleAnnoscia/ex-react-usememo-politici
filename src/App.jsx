191cfec788475313a680ce23be753a34f6d9bb49
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-09-09 15:59:07 +0200

    feat seconda milestone

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Card from './components/Card.jsx'
@@ -6,36 +6,48 @@ import Card from './components/Card.jsx'
 
 function App() {
   const [politicians, setPoliticians] = useState([])
+  const [search, setSearch] = useState("");
 
-  const fetchPoliticians =  async() => {
+  const fetchPoliticians = async () => {
     const response = await fetch(`http://localhost:3333/politicians`)
     const data = await response.json()
     setPoliticians(data)
   }
 
-  
-
+//funzione di rcicerca dei politici
+const filteredPoliticians = useMemo(()=>{
+  return politicians.filter( p => 
+    p.name.toLowerCase().includes(search.toLowerCase()) ||
+    p.biography.toLowerCase().includes(search.toLocaleLowerCase())
+  )
+}, [politicians, search])
   useEffect(() => {
-      fetchPoliticians()
-   }, []);
+    fetchPoliticians()
+  }, []);
 
 
-console.log(politicians)
+  console.log(politicians)
 
 
   return (
     <>
-    <div>
-        {politicians.map((politician, index)=> (
-           <Card 
-           key={index}
-           name = {politician.name}
-           image = {politician.image}
-           biography={politician.biography}
-           position={politician.position}
-            />
-        ))}
-    </div>
+      <div>
+        {/*sezione degli input di ricerca*/}
+        <input type="text"
+        placeholder ="Cerca per Nome o per Biografia"
+        value = {search}
+        onChange = {e => setSearch(e.target.value)} />
+
+        {filteredPoliticians.map((politician, index) => (
+        <Card
+          key={index}
+          name={politician.name}
+          image={politician.image}
+          biography={politician.biography}
+          position={politician.position}
+        />
+      ))}
+      </div>
     </>
   )
 }
