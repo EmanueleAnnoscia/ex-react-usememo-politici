da95f245c8835ce6aec118d3162ff1619fd7bd16
EmanueleAnnoscia <annosciaemanuele@gmail.com>
2025-09-09 16:22:09 +0200

    feat bonus

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,33 @@ import Card from './components/Card.jsx'
 function App() {
   const [politicians, setPoliticians] = useState([])
   const [search, setSearch] = useState("");
+  const [selectedPosition, setSelectedPosition] = useState("")
 
+  //chiamata api alle resources
   const fetchPoliticians = async () => {
     const response = await fetch(`http://localhost:3333/politicians`)
     const data = await response.json()
     setPoliticians(data)
   }
 
-//funzione di rcicerca dei politici
-const filteredPoliticians = useMemo(()=>{
-  return politicians.filter( p => 
-    p.name.toLowerCase().includes(search.toLowerCase()) ||
-    p.biography.toLowerCase().includes(search.toLocaleLowerCase())
-  )
-}, [politicians, search])
+  //funzione di rcicerca dei politici
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter(p =>
+      (p.name.toLowerCase().includes(search.toLowerCase()) ||
+        p.biography.toLowerCase().includes(search.toLowerCase())) &&
+      (selectedPosition === "" || p.position === selectedPosition)
+    )
+  }, [politicians, search, selectedPosition])
+
+
+
+  //ricerca su posizione politica
+  const positions = useMemo(() => {
+    const allPosition = politicians.map(p => p.position)
+    return [...new Set(allPosition)];
+  }, [politicians])
+
+  //use effect in carico sulla pagina
   useEffect(() => {
     fetchPoliticians()
   }, []);
@@ -34,22 +47,35 @@ const filteredPoliticians = useMemo(()=>{
       <div>
         {/*sezione degli input di ricerca*/}
         <input type="text"
-        placeholder ="Cerca per Nome o per Biografia"
-        value = {search}
-        onChange = {e => setSearch(e.target.value)} />
+          placeholder="Cerca per Nome o per Biografia"
+          value={search}
+          onChange={e => setSearch(e.target.value)} />
+
+        <select
+          value={selectedPosition}
+          onChange={e => setSelectedPosition(e.target.value)}
+        >
+          <option value="">Tutte le posizioni</option>
+          {positions.map((pos, i) => (
+            <option key={i} value={pos}>{pos}</option>
+          ))}
+        </select>
 
         {filteredPoliticians.map((politician, index) => (
-        <Card
-          key={index}
-          name={politician.name}
-          image={politician.image}
-          biography={politician.biography}
-          position={politician.position}
-        />
-      ))}
+          <Card
+            key={index}
+            name={politician.name}
+            image={politician.image}
+            biography={politician.biography}
+            position={politician.position}
+          />
+        ))}
       </div>
     </>
   )
 }
 
 export default App
+
+
+//return [...new Set(allPositions)]  me la spieghi passo passo
